5c855e094a5a71c7d24db506d64634dbce2ed5e3
pourhossinm <poouhossinm@gmail.com>
2025-02-19 10:29:31 +0300

    update 1-5

--- clinic/settings.py
+++ clinic/settings.py
@@ -123,9 +123,14 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+
+# مسیر ذخیره استاتیک فایل‌ها در Render
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# مسیر اضافی برای استاتیک فایل‌های داخل اپ‌ها
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
